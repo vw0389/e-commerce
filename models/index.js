fbb04073b825825a432b9dbef9ac11c3ba47b0cd
Victor Weinert <dev@vweinert.com>
2021-11-29 13:51:09 -0600

    removed unneccesary lines

--- models/index.js
+++ models/index.js
@@ -15,11 +15,11 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  
   foreignKey: "product_id"
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
+  through: ProductTag,
   foreignKey: "tag_id"
 });
 module.exports = {
