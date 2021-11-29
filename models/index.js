97117b85b1c1ad36a9d9cd9076d61af9f0785977
Victor Weinert <dev@vweinert.com>
2021-11-28 19:11:13 -0600

    models complete

--- models/index.js
+++ models/index.js
@@ -5,13 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "id"
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "id"
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: "id"
+});
 // Tags belongToMany Products (through ProductTag)
-
+TimeRanges.belongsToMany(Product, {
+  foreignKey: "id"
+});
 module.exports = {
   Product,
   Category,
