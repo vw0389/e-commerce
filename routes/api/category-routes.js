4e5edb2374d7d1ca27090a713186c68cb4e97af0
Victor Weinert <dev@vweinert.com>
2021-11-29 18:31:21 -0600

    complete all get and destroy routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,11 +4,50 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id",]
+      }
+    ]
+  }).then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({ message: 'No categories found' });
+      return;
+    }
+    res.json(categoryData);
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id",]
+      }
+    ]
+  }).then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that ID' });
+      return;
+    }
+    res.json(categoryData);
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // find one category by its `id` value
   // be sure to include its associated Products
 });
@@ -23,7 +62,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({ message: 'No tag found with that ID' });
+      return;
+    }
+    res.json(categoryData);
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,11 +4,51 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id",]
+      }
+    ]
+  }).then(tagData => {
+    if (!tagData) {
+      res.status(404).json({ message: 'No tags found' });
+      return;
+    }
+    res.json(tagData);
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+// find all tags
+// be sure to include its associated Product data
+
 
 router.get('/:id', (req, res) => {
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id",]
+      }
+    ]
+  }).then(tagData => {
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that ID' });
+      return;
+    }
+    res.json(tagData);
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
@@ -23,6 +63,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(tagData => {
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that ID' });
+      return;
+    }
+    res.json(tagData);
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
