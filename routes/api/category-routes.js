f4e27a8d6c607fb30755c90d92e925911b38d2cd
Victor Weinert <dev@vweinert.com>
2021-11-29 18:12:47 -0600

    added product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,6 +23,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,15 +5,63 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ["id", "category_name"]
+      },
+      {
+        model: Tag,
+        attributes: ["id", "tag_name"]
+      }
+    ]
+  }).then(productData => {
+    if (!productData) {
+      res.status(404).json({ message: 'No Products found' });
+      return;
+    }
+    res.json(productData);
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+// find all products
+// be sure to include its associated Category and Tag data
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ["id", "category_name"]
+      },
+      {
+        model: Tag,
+        attributes: ["id", "tag_name"]
+      }
+    ]
+  }).then(productData => {
+    if (!productData) {
+      res.status(404).json({ message: 'No product with this ID' });
+      return;
+    }
+    res.json(productData);
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+// find a single product by its `id`
+// be sure to include its associated Category and Tag data
+
 
 // create new product
 router.post('/', (req, res) => {
@@ -91,6 +139,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(productData => {
+      if (!productData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(productData);
+    }).catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
 
 module.exports = router;
